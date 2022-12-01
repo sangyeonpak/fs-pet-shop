f337c404ce0bbcbd285eecd2cb580b9d2cfbd50a
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-12-01 15:08:15 -0500

    Done with everything except bonuses

--- httpServer.js
+++ httpServer.js
@@ -6,17 +6,20 @@ const server = http.createServer((req, res) => {
 
 //=========================================================== /pets ===================================================================================
 
+// shorthand { method, url } = req; then just type instead of method instead of req.method
   if (req.method === "GET" && req.url === "/pets"){
     readFile("./pets.json", "utf-8").then((text) =>{
       res.setHeader("Content-Type", "application/json");
       res.end(text);
     })
 
-//=========================================================== /pets/index== ===========================================================================
+//=========================================================== /pets/index =============================================================================
 
   } else if (req.method === "GET" && (req.url).startsWith("/pets/")){
     let index = (req.url).substring(6);
     console.log(index);
+
+    // if index is defined
     if (index != undefined){
     readFile("./pets.json", "utf-8").then((text) =>{
       res.setHeader("Content-Type", "application/json");
@@ -24,10 +27,23 @@ const server = http.createServer((req, res) => {
       const petsParsed = JSON.parse(text);
       console.log(petsParsed[index]);// gotta re-convert to string?
       const petAtIndexString = JSON.stringify(petsParsed[index]);
-      res.end(petAtIndexString);
-    })
-    } else{
 
+      // if index results in a match
+      if (petAtIndexString != undefined){
+        res.end(petAtIndexString);
+
+      // index doesn't result in a match
+      } else{
+        res.setHeader("Content-Type", "text/plain");
+        res.statusCode = 404;
+        res.end("Not Found");
+      }
+    })
+    // if index is undefined
+    } else{
+      res.setHeader("Content-Type", "text/plain");
+      res.statusCode = 404;
+      res.end("Not Found");
     }
 
 //=========================================================== anything else ===========================================================================
