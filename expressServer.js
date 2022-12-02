52a33612bb4c53cb58198f7297731325993e8deb
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-12-02 17:08:39 -0500

    i was doing it wrong this whole time lol, fixed it so that it gets data from the body

--- expressServer.js
+++ expressServer.js
@@ -3,7 +3,9 @@ import { readFile, writeFile } from "node:fs/promises";
 
 const server = express();
 const port = 3000;
-function c(x) { console.log(x)};
+function c(x) { console.log(x)}; // robin's console.log shortcut
+
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(404).send("Not Found")
@@ -31,29 +33,73 @@ server.get("/pets/:index", (req, res) => {
   })
 });
 
-server.post("/pets%20:info", (req, res) => {
-  const info = req.params.info;
-  c(info);
-  const infoSep = (info.split(' '));
-  c(infoSep);
-  for (let i = 0; i<3; i++){
-    infoSep[i] = infoSep[i].split('=');
-  }
-  const pet = Object.fromEntries(infoSep);
-  c(pet);
-  // c(infoSep);
-  // const pet = { 'age':Number(age), 'kind':kind, 'name':name };
-  // console.log(pet);
+server.post("/pets", (req, res) => {
+  const pet = req.body;
+  if (Object.keys(pet).length === 3){
+    if ('age' in pet && 'kind' in pet && 'name' in pet){ // PASS: age, kind, and name are the keys
+      pet.age = Number(pet.age);
+      if (isNaN(pet.age) || pet.age === '' || pet.kind === '' || pet.name === ''){ // FAIL: see else statement; wonder why pet.age === NaN didn't work
+        res.status(400).send("Bad request, noob")
 
-  // c(urlParams);
-  // readFile("./pets.json", "utf-8").then((text) =>{
+
+
+      } else{ // PASS: age is not NAN, age is not blank, kind is not blank, or name is not blank
+        readFile("./pets.json", "utf-8").then((text)=>{
+          const pets = JSON.parse(text);
+          pets.push(pet);
+          writeFile("./pets.json", JSON.stringify(pets)).then((error) =>{
+            error ? c("Something went wrong: ", error) : c('Pet recorded in our database: ', pet);
+            res.json(pet);
+          })
+        })
+      }
+
+
+
+    } else{ // FAIL: age, kind, and name are NOT the keys
+      res.status(400).send("Bad request, noob")
+    }
+  } else{ // FAIL: more or less than 3 entries
+    res.status(400).send("Bad request, noob")
+  }
+  // readFile("./pets.json", "utf-8").then((text)=>{
   //   const pets = JSON.parse(text);
   //   pets.push(pet);
-  //   writeFile("./pets.json", pets).then((err)=>{
-  //     err ? c('Something went wrong: ', err) : c('Pet recorded in our database!')
+  //   writeFile("./pets.json", JSON.stringify(pets), (error)=>{
+  //     error ? res.send(error) : res.json(pets);
   //   })
   // })
-  // res.send(petRegExp.exec())
+  // const info = req.params.info;
+  // c(req.body);
+
+  // const infoSep = (info.split(' '));
+  // for (let i = 0; i<infoSep.length; i++){
+  //   infoSep[i] = infoSep[i].split('=');
+  // }
+  // const pet = Object.fromEntries(infoSep); // c(Object.keys(pet).length);
+  // req.body = pet;
+  // c(req.body);
+  // if (Object.keys(pet).length === 3){
+  //   if ('age' in pet && 'kind' in pet && 'name' in pet){ // PASS: age, kind, and name are the keys
+  //     pet.age = Number(pet.age);
+  //     if (isNaN(pet.age) || pet.age === '' || pet.kind === '' || pet.name === ''){ // FAIL: see else statement; wonder why pet.age === NaN didn't work
+  //       res.status(400).send("Bad request, noob")
+  //     } else{ // PASS: age is not NAN, age is not blank, kind is not blank, or name is not blank
+  //       readFile("./pets.json", "utf-8").then((text)=>{
+  //         const pets = JSON.parse(text);
+  //         pets.push(pet);
+  //         writeFile("./pets.json", JSON.stringify(pets)).then((error) =>{
+  //           error ? c("Something went wrong: ", error) : c('Pet recorded in our database: ', pet);
+  //           res.json(pet);
+  //         })
+  //       })
+  //     }
+  //   } else{ // FAIL: age, kind, and name are NOT the keys
+  //     res.status(400).send("Bad request, noob")
+  //   }
+  // } else{ // FAIL: more or less than 3 entries
+  //   res.status(400).send("Bad request, noob")
+  // }
 })
 
 server.listen(port, ()=>{
